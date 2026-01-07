f8a702c907eaca6477908a1425532f752df8bf27
alpiazza13 <alexpiazza2000@gmail.com.com>
2026-01-07 17:39:03 -0500

    initial frontend page with list of matches

--- docs/frontend/initial_front_end_set_up.md
+++ docs/frontend/initial_front_end_set_up.md
@@ -1,4 +1,25 @@
 1. Install node using `brew install node`
 2. Initialize project by running, from root of project, command `npx create-next-app@latest frontend`. Select Use recommended defaults when prompted. Options to select:
+3. Add code to main.py in backend so backend can receive requests from frontend
+	```python
 	
+	from fastapi.middleware.cors import CORSMiddleware
 	
+	#right under app = FastAPI() line
+	app.add_middleware(
+
+		CORSMiddleware,
+		
+		allow_origins=["http://localhost:3000"],
+		
+		allow_credentials=True,
+		
+		allow_methods=["*"],
+		
+		allow_headers=["*"],
+
+)
+	```
+	
+4. Create `.env.local` file in root of `frontend` folder, add `NEXT_PUBLIC_API_URL=http://localhost:8000`
+5. Start writing typescript code
---
+++ frontend/types/matches.ts
@@ -0,0 +1,40 @@
+export interface Competition {
+  id: number;
+  external_id: number;
+  name: string;
+  code: string;
+  type: string;
+  emblem: string | null;
+}
+
+export interface Team {
+  id: number;
+  external_id: number;
+  name: string;
+  tla: string;
+  crest: string | null;
+}
+
+export interface Match {
+  id: number;
+  external_id: number;
+  utc_date: string;
+  status: string;
+  matchday: number;
+  stage: string;
+  group: string | null;
+  last_updated: string;
+  score: any; // score is json column
+  home_team: Team;
+  away_team: Team;
+  competition_id: number;
+}
+
+export interface UserMatch {
+  id: number;
+  user_id: number;
+  match_id: number;
+  is_done: boolean;
+  notes: string | null;
+  match?: Match;
+}
