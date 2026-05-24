c7e4f521dfe39614acdd091532b0341ff765a726
alpiazza13 <alexpiazza2000@gmail.com.com>
2026-05-23 23:50:18 -0400

    remove trailing slash from amiplify url

--- backend/app/main.py
+++ backend/app/main.py
@@ -44,7 +44,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://main.d1hthutgtmryew.amplifyapp.com/"],
+    allow_origins=["http://localhost:3000", "https://main.d1hthutgtmryew.amplifyapp.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
