2e6a165c6c789136822e504b07bcd7ac3044a24b
alpiazza13 <alexpiazza2000@gmail.com.com>
2026-05-28 22:11:50 -0400

    add matchesqueue.com to allow origins list

--- backend/app/main.py
+++ backend/app/main.py
@@ -48,7 +48,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://main.d1hthutgtmryew.amplifyapp.com"],
+    allow_origins=["http://localhost:3000",
+                   "https://matchesqueue.com",
+                   "https://www.matchesqueue.com", 
+                   "https://main.d1hthutgtmryew.amplifyapp.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
